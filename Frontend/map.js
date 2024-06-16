e8027182503a1fb761ae98fcaa01bc35f68cc918
DrakeH5 <Newbuild100@outlook.com>
2024-06-16 17:39:04 -0500

    map

---
+++ Frontend/map.js
@@ -0,0 +1,8 @@
+maptilersdk.config.apiKey = 'JmXBghQQLs4ZM5NeVxab';
+const map = new maptilersdk.Map({
+  container: 'map', // container's id or the HTML element to render the map
+  style: "675ac197-2170-4e29-b084-6f1d9dd39c82",
+  center: [-97, 35], // starting position [lng, lat]
+  zoom: 3.5, // starting zoom
+});
+
